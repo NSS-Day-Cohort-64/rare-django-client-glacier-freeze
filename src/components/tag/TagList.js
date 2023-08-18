06e29857b95180a8b670f86fc67e20a475be9307
Cassie Revell <classierev@gmail.com>
2023-08-18 14:55:24 -0500

    Completed Tag Tickets: Create, Edit, Delete

--- src/components/tag/TagList.js
+++ src/components/tag/TagList.js
@@ -1,55 +1,98 @@
 import { useState, useEffect } from "react"
-import { getTags } from "../../managers/TagManager"
+import { useParams } from "react-router-dom";
+import { getTags, deleteTags } from "../../managers/TagManager"
 import { CreateTag } from "./CreateTag"
+import { EditTag } from "./EditTag"
+
 
 export const TagList = () => {
   const [tags, setTags] = useState([])
+  const [selectedTag, setSelectedTag] = useState(null)
+  const { tagId } = useParams()
   const [showForm, updateShowForm] = useState(false)
+  const [showEditForm, setShowEditForm] = useState(false);
+
 
   const updateTags = () => {
     getTags().then(tagData => setTags(tagData))
   }
 
+
   useEffect(() => {
-      getTags().then(tagData => setTags(tagData))
+    getTags().then(tagData => setTags(tagData))
   }, [])
-
-    const deleteButton = () => {
-        return (<button>Delete </button>)
-        } 
-
-    const editButton = () => {
-        return(<button> Edit</button>)
+  
+  
+  const deleteButton = (tagId) => {
+    const handleDelete = () => {
+      const shouldDeleteTag = window.confirm("Are you sure you want to delete this tag?");
+      if (shouldDeleteTag) {
+        deleteTags(tagId)
+      }
     }
+
+    return (
+      <button onClick={handleDelete}>
+        Delete
+      </button>
+    )
+  }
+  
+  const editButton = (tag) => {
+    return (<button onClick={() => handleEditTag(tag)}>Edit</button>)
+  }
+  
+    const handleEditTag = (tag) => {
+      setSelectedTag(tag)
+      setShowEditForm(true)
+    }
+
+  
+    const handleCancelEdit = () => {
+      setSelectedTag(null)
+    };
 
   return (
     <article className="is-flex is-justify-content-space-evenly">
-    <div style={{ margin: "0rem 3rem" }} className="column is-two-thirds">
+      <div style={{ margin: "0rem 3rem" }} className="column is-two-thirds">
         <h1>Tags</h1>
 
         <div className="tags column is-full is-justify-content-space-evenly">
-            {tags.map((tag) => (
-                <section className="tag" key={tag?.id}>
-                  <div className="tagLabel">{tag?.label}</div>
-                  <footer>{deleteButton()}
-                  {editButton()}</footer>
-                </section>
-              ))}
+          {tags.map((tag) => (
+            <section className="tag" key={tag?.id}>
+              <div className="tagLabel">{tag?.label}</div>
+              <footer>{deleteButton(tag.id)}
+                {editButton(tag)}</footer>
+            </section>
+          ))}
         </div>
-    </div>
+      </div>
 
-    <section className="createTag column">
-          {
-            showForm
-              ? <CreateTag
-                updateShowForm={updateShowForm}
-                tagList={tags}
-                updateTags={updateTags} />
-              : <button className="showCreateTag"
-                onClick={click => updateShowForm(!showForm)}
-              >Create New</button>
-          }
-          </section>
+      <section className="createTag column">
+        {
+          showForm
+            ? <CreateTag
+              updateShowForm={updateShowForm}
+              tagList={tags}
+              updateTags={updateTags} />
+            : <button className="showCreateTag"
+              onClick={click => updateShowForm(!showForm)}
+            >Create New</button>
+        }
+      </section>
+      <section className="editTag column">
+        {showEditForm && selectedTag && (
+          <EditTag
+            tag={selectedTag}
+            onCancel={handleCancelEdit}
+            onUpdate={(updatedTag) => {
+              updateTags()
+              setShowEditForm(false)
+            }}
+          />
+        )}
+      </section>
+
     </article>
   )
 }
