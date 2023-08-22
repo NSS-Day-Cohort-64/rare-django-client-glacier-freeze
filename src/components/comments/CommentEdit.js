1d19f0bae36a80742000ea0e1fc98cf6b1242ce6
Daniel Myers <danielm53861991@gmail.com>
2023-08-22 09:11:34 -0500

    edit comments

--- src/components/comments/CommentEdit.js
+++ src/components/comments/CommentEdit.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
-import { getGames } from "../../managers/GameManager";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const EditComment = () => {
     const [newComment, setComment] = useState({
@@ -9,90 +8,73 @@ export const EditComment = () => {
         content: ""
     });
 
-    const { commentId } = useParams();
+    const { commentId, postId } = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
-        fetch(`http://localhost:8000/comments/${parseInt(commentId)}`, {
-
+        // Fetch comment data when the component mounts
+        fetch(`http://localhost:8000/comments/${commentId}`, {
             headers: {
-                "Authorization": `Token ${localStorage.getItem("lu_token")}`
-
+                "Authorization": `Token ${localStorage.getItem("auth_token")}`
             }
         })
-            .then(response => response.json())
-            .then(data => {
-                setComment(data); // Use setGame to update the state
-            });
+        .then(response => response.json())
+        .then(data => {
+            setComment(data); // Update the state with the fetched comment data
+        });
     }, []);
 
     const changeCommentState = (domEvent) => {
-
-        const updatedComment = Object.assign({}, newComment)
-        updatedComment[domEvent.target.name] = domEvent.target.value
-        setComment(updatedEvent)
-
+        // Update the specific field in the newComment state
+        const updatedComment = { ...newComment };
+        updatedComment[domEvent.target.name] = domEvent.target.value;
+        setComment(updatedComment);
     }
 
     return (
-        <form className="EventForm">
-            <h2 className="eventForm__title">Amend Board Game Night</h2>
+        <form className="CommentForm">
+            <h2 className="commentForm__title">Post Details</h2>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="date">Date: </label>
-                    <input type="date" name="date" required autoFocus className="form-control"
-                        selected={newEvent.date}
-                        onChange={changeEventState}
+                    <label htmlFor="comment">Content: </label>
+                    {/* Use the correct 'name' attribute and value from the state */}
+                    <input
+                        type="text"
+                        name="content" // Use the correct name to match the state property
+                        required
+                        autoFocus
+                        className="form-control"
+                        value={newComment.content} // Bind the value from the state
+                        onChange={changeCommentState}
                     />
                 </div>
-
             </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="game">Game: </label>
-                    <select
-                        value={newEvent.game}
-                        name="game"
-                        onChange={changeEventState}
-                    >
-                        <option value="0"> Select Your Type</option>
-                        {games.map((game) => (
-                            <option
-                                key={`type--${game.id}`}
-                                value={game.id}
-                            >
-                                {game.title}
-                            </option>
-
-                        ))}
-                    </select>
-                </div>
-            </fieldset>
-            <button type="submit"
+            <button
+                type="submit"
                 onClick={evt => {
-                    // Prevent form from being submitted
-                    evt.preventDefault()
-
-                    const newestEvent = {
-                        game: parseInt(newEvent.game),
-                        date: newEvent.date
-
-                    }
-
-                    fetch(`http://localhost:8000/events/${eventId}`, {
+                    evt.preventDefault(); // Prevent form submission
+                    const newestComment = {
+                        post: newComment?.post?.id, // Use the correct property from the state
+                        author: newComment?.author?.id, // Use the correct property from the state
+                        content: newComment.content
+                    };
+                    // Send the updated comment data to the server
+                    fetch(`http://localhost:8000/comments/${commentId}`, {
                         method: "PUT",
                         headers: {
                             "Content-Type": "application/json",
-                            Authorization: `Token ${localStorage.getItem("lu_token")}`
+                            Authorization: `Token ${localStorage.getItem("auth_token")}`
                         },
-                        body: JSON.stringify(newestEvent)
+                        body: JSON.stringify(newestComment)
                     })
-                        .then(() => navigate("/events"));
+                    .then(() => navigate(`/comments/${newComment?.post?.id}`)); // Navigate after successful update
                 }}
                 className="btn btn-primary"
             >
-                Save Edits
-            </button>
+                Save
+            </button> 
+            <button onClick={()=>navigate(`/comments/${newComment?.post?.id}`)}> Cancel </button>
         </form>
     );
 };
+
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -16,6 +16,7 @@ import { CommentForm } from "../components/comments/CommentForm"
 
 
 import { ReactionList } from "../components/reactions/reactionList.js"
+import { EditComment } from "../components/comments/CommentEdit"
 
 //import { SubscribedUserPosts } from "../components/subscriptions/ViewSubscribedUserPosts"
 
@@ -44,9 +45,12 @@ export const ApplicationViews = ({ token, setToken}) => {
         </Route>
         <Route path="/postform" element={<PostForm token={token}/>}  />
         <Route path="/my-posts/:postId/edit" element={<PostEdit />}  />
+        <Route path="/comments/:commentId/edit" element={<EditComment />}  />
+
  
 
       </Route>
     </Routes>
   </>
 }
+
